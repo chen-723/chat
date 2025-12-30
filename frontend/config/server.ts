baccaa6df71f1302dbcb8775bdcc8faf7b0da684
cqj123 <1751249755@qq.com>
2025-12-30 15:03:32 +0800

    1

--- frontend/config/server.ts
+++ frontend/config/server.ts
@@ -5,7 +5,7 @@ const SERVER_CONFIG = {
   // 开发环境使用 localhost
   // 生产环境改为服务器IP，例如: 'http://192.168.2.19:8000'
   // API_BASE_URL: 'https://localhost:8000',
-  API_BASE_URL: 'https://192.168.2.38:8000',
+  API_BASE_URL: 'https://209.74.81.100:8000',
   
   // WebSocket地址（自动根据API地址生成）
   get WS_BASE_URL() {
